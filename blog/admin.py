c4997130a344431657c0f6d77b85537ab313ce48
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-01-14 17:36:44 -0500

    Created and registered models for weekly blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WeeklyBlogPost, Content, Text, Code_S, Code_M, Code_L, Image
 
-# Register your models here.
+admin.site.register(WeeklyBlogPost)
+admin.site.register(Content)
+admin.site.register(Text)
+admin.site.register(Code_S)
+admin.site.register(Code_M)
+admin.site.register(Code_L)
+admin.site.register(Image)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class WeeklyBlogPost(models.Model):
+    title = models.CharField(max_length = 200)
+    thumbnail = models.ImageField(upload_to = 'static/blog')
+    creation_time = models.DateTimeField(default = timezone.now)
+
+class Content(models.Model):
+    blog = models.ForeignKey(WeeklyBlogPost, on_delete = models.CASCADE)
+    content_type = models.CharField(max_length = 10)
+
+#Content types
+class Text(models.Model):
+    content_container = models.ForeignKey(Content, on_delete = models.CASCADE)
+    text = models.CharField(max_length = 500)
+
+class Code_S(models.Model):
+    content_container = models.ForeignKey(Content, on_delete = models.CASCADE)
+    code = models.CharField(max_length = 4000)
+    
+class Code_M(models.Model):
+    content_container = models.ForeignKey(Content, on_delete = models.CASCADE)
+    code = models.CharField(max_length = 20000)
+
+class Code_L(models.Model):
+    content_container = models.ForeignKey(Content, on_delete = models.CASCADE)
+    code = models.CharField(max_length = 100000)
+
+class Image(models.Model):
+    content_container = models.ForeignKey(Content, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to = 'static/blog')
