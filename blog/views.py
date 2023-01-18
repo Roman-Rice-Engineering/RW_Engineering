6a690e0fb0d1f83aa0c6e8b100b075c74d5a82b4
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-01-18 14:01:57 -0500

    Created feature allowing to view blogs
    Blogs preview
    --Fully functional
    --Displays preview text, image, and time ago posted
    --Superusers can delete blog posts, these blogs are recoverable
    
    Individual blog viewing
    --Not complete
    --Only supports text currently

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import WeeklyBlogPost
 
 def blog(request):
-    return render(request, 'blog/blog.html', {})
+    if request.method == "POST":
+        if request.POST.get('delete_blog') and request.user.is_superuser:
+            blog = WeeklyBlogPost.objects.get(id = request.POST.get('delete_blog'))
+            blog.deleted = True
+            blog.save()
+        return HttpResponseRedirect('/blog')
+    BlogClass = WeeklyBlogPost.objects.all()
+    return render(request, 'blog/blog.html', {'AllBlogs': BlogClass})
+
+def blog_post(request, id):
+    if not WeeklyBlogPost.objects.filter(id = id).exists():
+        return HttpResponse('Error, blog post does not exist')
+    blog = WeeklyBlogPost.objects.get(id = id)
+    return render(request, 'blog/blog_post.html', {'blog': blog})
